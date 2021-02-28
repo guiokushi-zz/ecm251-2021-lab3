ceb5256a066ec017c2facdfc926ac0be3229532a
guiokushi <guiokushi@hotmail.com>
2021-02-28 18:07:26 -0300

    Alteração na base

---
+++ Aula01/prog1/OlaMundo.java
@@ -0,0 +1,13 @@
+class OlaMundo {
+    public static void main(String[] args) {
+        //Isso é um comentario de uma linha
+        System.out.println("Hello World!");
+        System.out.println("Stein é mal?");
+        System.out.println("Overwatch morreu?");
+        System.out.println("Aqui!");
+        /*
+        * Comentario 
+        * de mais linhas
+        */
+    }    
+}
